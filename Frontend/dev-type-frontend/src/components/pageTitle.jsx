573149b4bedc42fe3b657f6d9f647fab49a026d9
Jose Luis Lobera <40039535+JHludwolf@users.noreply.github.com>
2023-08-16 13:50:23 -0600

    Create pageTitle.jsx

---
+++ Frontend/dev-type-frontend/src/components/pageTitle.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "../style/home/home.css";
+import { useSelector } from "react-redux";
+
+const PageTitle = () => {
+    const colors = useSelector((state) => state.theme.colors);
+
+	return (
+		<div>
+			<h1 className="home-h1" style={{ color: colors.accent }}>
+				DevType
+			</h1>
+			<h2 className="home-h2" style={{ color: colors.text }}>
+				A typing test for developers
+			</h2>
+			<h3 className="home-h3" style={{ color: colors.text }}>
+				By Dinamita &copy;{" "}
+			</h3>
+		</div>
+	);
+};
+
+export default PageTitle;
